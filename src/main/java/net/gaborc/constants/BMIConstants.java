6f99495ac1e4e5a4bb738cf9cbadecabc011bb96
Danis Gábor <danisg@jusoft.hu>
2017-11-02 21:37:55 +0100

    Defined basic constants for BMI

---
+++ src/main/java/net/gaborc/constants/BMIConstants.java
@@ -0,0 +1,11 @@
+package net.gaborc.constants;
+
+/**
+ * Constants used for BMI calculations
+ */
+public class BMIConstants {
+    public final static double MIN_HEIGHT_IN_CENTIMETERS = 20;
+    public final static double MAX_HEIGHT_IN_CENTIMETERS = 250;
+    public final static double MIN_WEIGHT_IN_KILOGRAMS = 10;
+    public final static double MAX_WEIGHT_IN_KILOGRAMS = 500;
+}
